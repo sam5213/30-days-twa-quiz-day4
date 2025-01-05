b1674a7c8fa2e18ea394b679d03497265012c3d3
sam5213 <gulmirazhb@gmail.com>
2025-01-05 21:06:35 +0500

    Update script.js to start

--- script.js
+++ script.js
@@ -12,72 +12,24 @@ const questions = [
 ];
 
 let currentQuestionIndex = 0;
-let timer;
-const timeLimit = 15; // Время на ответ в секундах
+
 
 function startQuiz() {
-    currentQuestionIndex = 0;
-    document.getElementById('app').style.display = 'none';
-    document.getElementById('quiz').style.display = 'block';
-    askQuestion();
+    //TODO
 }
 
 function askQuestion() {
-    const question = questions[currentQuestionIndex];
-    document.getElementById('question').innerText = question.question;
-
-    const optionsDiv = document.getElementById('options');
-    optionsDiv.innerHTML = '';
-
-    question.options.forEach(option => {
-        const button = document.createElement('button');
-        button.innerText = option;
-        button.onclick = () => checkAnswer(option, question.answer);
-        optionsDiv.appendChild(button);
-    });
-
-    startTimer();
+    //TODO
 }
 
 function startTimer() {
-    let timeLeft = timeLimit;
-    document.getElementById('time').innerText = timeLeft;
-
-    timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById('time').innerText = timeLeft;
-
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            alert("Время вышло! Переход к следующему вопросу.");
-            currentQuestionIndex++;
-            if (currentQuestionIndex < questions.length) {
-                askQuestion();
-            } else {
-                alert("Квиз завершен! Спасибо за участие.");
-                resetQuiz();
-            }
-        }
-    }, 1000);
+    //TODO
 }
 
 function checkAnswer(selected, correct) {
-    clearInterval(timer); // Останавливаем таймер
-    if (selected === correct) {
-        alert("Правильно! Молодец!");
-    } else {
-        alert("Неправильно. Попробуй еще раз!");
-    }
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        askQuestion();
-    } else {
-        alert("Квиз завершен! Спасибо за участие.");
-        resetQuiz();
-    }
+    //TODO
 }
 
 function resetQuiz() {
-    document.getElementById('quiz').style.display = 'none';
-    document.getElementById('app').style.display = 'block';
+    //TODO
 }
