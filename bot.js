b5cf9e54039ca99b40cc8241c19a1ba0f4ba62fa
sam5213 <gulmirazhb@gmail.com>
2025-01-05 21:50:38 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN_30DAYS);
 bot.start((ctx) => {
     ctx.reply("Привет! Давай сыграем в квиз с несклькими вопросами с таймером. ", Markup.inlineKeyboard([
         Markup.button.webApp("Присупить к квизу", "https://sam5213.github.io/30-days-twa-quiz-day4")]
-    ));
+    ))
 });
 
 
