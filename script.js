5cefe08c99690c44b5526d061e70b790374c52cc
sam5213 <gulmirazhb@gmail.com>
2025-01-05 21:45:15 +0500

    Update script.js to launch

--- script.js
+++ script.js
@@ -12,24 +12,73 @@ const questions = [
 ];
 
 let currentQuestionIndex = 0;
+let timer;
+const timeLimit = 15;
 
 
 function startQuiz() {
-    //TODO
+    currentQuestionIndex = 0;
+    document.getElementById('app').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
+    askQuestion();
 }
 
 function askQuestion() {
-    //TODO
+    const question = questions[currentQuestionIndex];
+    document.getElementById('question').innerText = question.question;
+
+    const optionsDiv = document.getElementById('options');
+    optionsDiv.innerHTML = '';
+
+    questions.options.forEach(option => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.onclick = () => checkAnswer(option, question.answer);
+        optionsDiv.appendChild(button);
+    });
+
+    startTimer();
 }
 
 function startTimer() {
-    //TODO
+    let timeLeft = timeLimit;
+    document.getElementById('time').innerText = timeLeft;
+
+    timer = setInterval(() => {
+        timeLeft--;
+        document.getElementById('time').innerText = timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            alert("Время вышло! Переход к ледующему вопросу.");
+            currentQuestionIndex++;
+            if (currentQuestionIndex < question.length) {
+                askQuestion();
+            } else {
+                alert("Квиз завершен! спасибо за участие.");
+                resetQuiz();
+            }
+        }
+    }, 1000);
 }
 
 function checkAnswer(selected, correct) {
-    //TODO
+    clearInterval(timer);
+    if (selected === correct) {
+        alert("Правильно!");
+    } else {
+        alert("Неправильно!");
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex < question.length) {
+        askQuestion();
+    } else {
+        alert("Квиз завершен! спасибо за участие.");
+        resetQuiz();
+    }
 }
 
 function resetQuiz() {
-    //TODO
+    document.getElementById('app').style.display = 'none';
+    document.getElementById('quiz').style.display = 'block';
 }
